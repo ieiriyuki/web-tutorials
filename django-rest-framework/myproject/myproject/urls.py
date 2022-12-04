03f0b01c90522dc485841f31d7d838272039620b
ieiriyuki <ieiri@example>
2022-12-04 09:25:34 +0900

    feat: view for snippets

--- django-rest-framework/myproject/myproject/urls.py
+++ django-rest-framework/myproject/myproject/urls.py
@@ -3,12 +3,14 @@ from django.urls import include, path
 from rest_framework import routers
 from myproject.foo import views
 
+"""
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+"""
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include('snippets.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
