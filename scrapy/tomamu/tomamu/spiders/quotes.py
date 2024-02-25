1d1cff9e5647c7e455e9f7d06afbaeee7533a2aa
ieiriyuki <ieiri@example>
2024-02-25 10:35:07 +0900

    feat: parse url and yield item

--- scrapy/tomamu/tomamu/spiders/quotes.py
+++ scrapy/tomamu/tomamu/spiders/quotes.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import scrapy
 
+from tomamu.items import TomamuItem
+
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -15,7 +17,11 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"saved file {filename}")
+        for quote in response.css("div.quote"):
+            item = TomamuItem(
+                text=quote.css("span.text::text").get(),
+                author=quote.css("small.author::text").get(),
+                tags=quote.css("div.tags a.tag::text").getall(),
+            )
+            yield item
+        self.log(f"an item was obttained from {response.url}")
