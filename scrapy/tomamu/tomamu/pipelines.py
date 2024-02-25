ee6b7e02623d90860ebe705b6a23d82afa9a2c6c
ieiriyuki <ieiri@example>
2024-02-25 23:30:29 +0900

    refactor: export with exporter

--- scrapy/tomamu/tomamu/pipelines.py
+++ scrapy/tomamu/tomamu/pipelines.py
@@ -2,16 +2,22 @@
 import json
 
 from itemadapter import ItemAdapter
+from scrapy.exporters import JsonLinesItemExporter
 
 
 class TomamuPipeline:
     def open_spider(self, spider):
-        self.file = open("quotes.jsonl", "w")
+        self.file = open("quotes.jsonl", "wb")
 
     def close_spider(self, spider):
         self.file.close()
 
     def process_item(self, item, spider):
-        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
-        self.file.write(line)
+        self._exporter_for_items(item)
         return item
+
+    def _exporter_for_items(self, item):
+        exporter = JsonLinesItemExporter(self.file)
+        exporter.start_exporting()
+        exporter.export_item(item)
+        exporter.finish_exporting()
