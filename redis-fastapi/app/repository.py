39015195c9b419d6c372ec8209e6cef1aec7346c
ieiriyuki <ieiriyuki@example.com>
2025-05-22 08:49:57 +0000

    refactor: use hset to reproduce tran exam

--- redis-fastapi/app/repository.py
+++ redis-fastapi/app/repository.py
@@ -6,18 +6,28 @@ class CreditRepository:
         self.reader = reader
         self.writer = writer
 
+    def key(self, id: int):
+        return f"{id}"
+
     def get_credit(self, id: int):
-        amount = self.reader.get(id)
+        value = self.reader.hgetall(self.key(id))
+        amount = value.get("amount")
         if amount is not None:
             return int(amount)
         else:
             return None
 
     def set_credit(self, id: int, amount: int):
-        self.writer.set(id, amount)
+        self.writer.hset(self.key(id), mapping=self.mapping(amount))
 
     def add_credit(self, id: int, amount: int):
-        self.reader.incrby(id, amount)
+        value = self.reader.hgetall(self.key(id))
+        current_amount = value.get("amount")
+        if current_amount is not None:
+            new_amount = int(current_amount) + amount
+        else:
+            new_amount = amount
+        self.reader.hset(self.key(id), mapping=self.mapping(new_amount))
 
     def swap_db(self):
         self.reader.swapdb(
@@ -26,8 +36,11 @@ class CreditRepository:
         )
 
     def initialize_credit(self):
-        for id in range(0, 10):
-            self.writer.set(id, 1000)
+        for id in range(0, 5):
+            self.set_credit(id, 1000)
 
         self.swap_db()
         self.writer.flushdb()
+
+    def mapping(self, amount: int):
+        return {"amount": amount}
